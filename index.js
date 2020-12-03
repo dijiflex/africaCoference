cf9d3108370fd03d2e3834e8a367870b142d043a
dijiflex <dijiflex@gmail.com>
2020-12-03 17:46:14 +0300

    Deleted yalm file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 const express = require('express');
 const app = express();
+ 
+
 
 app.get('/', (req, res) => {
 
     res.send(req.query.name + 'Hello from the Microsoft Student Summit')
 })
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log('The applicaiton is running. 😁');
