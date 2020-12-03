7c4eda71a5b6f42dbbe9df8a65a5f0fa14c2839e
dijiflex <dijiflex@gmail.com>
2020-12-03 15:45:55 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+
+    res.send(req.query.name + 'Hello from the Microsoft Student Summit')
+})
+
+app.listen(3000);
+console.log('The applicaiton is running. 😁');
